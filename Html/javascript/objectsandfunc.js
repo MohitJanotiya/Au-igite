bcd7c86f6d22edc8f3f86e23eb3a4309b53e3f07
Mohit Janotiya <123796513+MohitJanotiya@users.noreply.github.com>
2026-01-13 16:52:45 +0530

    oaf

---
+++ Html/javascript/objectsandfunc.js
@@ -0,0 +1,40 @@
+
+let user = {
+    name: "Mohit",
+    age: 23
+};
+console.log(user.name);
+
+
+let car = {
+    brand: "BMW",
+    speed: 120,
+    drive: function () {
+        console.log("Driving at " + this.speed + " km/h");
+    }
+};
+car.drive();
+
+
+
+
+function add(a, b) {
+    return a + b;
+}
+console.log(add(10, 20));
+
+
+const square = (n) => n * n;
+console.log(square(5));
+
+
+
+let div = document.getElementById("container");
+div.innerText = "Hello Script";
+div.style.color = "red";
+
+let b = document.getElementById("btn");
+
+b.addEventListener("click", function () {
+    alert("Button clicked");
+});
